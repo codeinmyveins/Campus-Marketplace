a92cf1f11734bb43dbbea94aec99ac10873b4576
VantaTree <VantaTree@gmail.com>
2025-04-07 01:13:00 +0530

    removed test code

--- app.js
+++ app.js
@@ -43,19 +43,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/user', async (req, res) => {
-
-    try {
-        const newUser = await pool.query("INSERT INTO users (username, full_name, email, password, dob, country_code, phone, college_name, gender) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING to_char(dob, \'DD-MM-YYYY\') as dob_date, *",
-            ["me1", "my name", "email@email.com", "123456789", "2004/11/11", "IN", "+91 9849248704", "my college", "male"]
-        )
-        res.status(200).json({ newUser });
-    } catch (error) {
-        res.status(200).json({ error });
-    }
-
-});
-
 app.get("/surprise", (req, res) => {
     res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 });
