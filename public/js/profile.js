e964033b2974e25bece5ea34f22e9cefd44740ba
sohail-1209 <sohailpashe@gmail.com>
2025-04-16 15:03:50 +0530

    Added public profile page

---
+++ public/js/profile.js
@@ -0,0 +1,38 @@
+const username = new URLSearchParams(window.location.search).get("u");
+
+async function getCountryNameFromCode(code) {
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/alpha/${code}`);
+    const country = response.data[0];
+    return country.name.common;
+  } catch (error) {
+    console.error('Error fetching country:', error.message);
+  }
+}
+
+async function fillUserDetails(){
+  const pfpDOM = document.getElementById("pfp");
+  const fullnameDOM = document.getElementById("fullname");
+  const usernameDOM = document.getElementById("username");
+  const bioDOM = document.getElementById("bio");
+  const countryDOM = document.getElementById("country");
+  const collegeDOM = document.getElementById("college");
+
+  try {
+    const { data: { user } } = await axios.get("/api/users/"+username);
+    if (user.avatar_url)
+      pfpDOM.src = user.avatar_url;
+    fullnameDOM.textContent = user.full_name;
+    countryDOM.textContent = await getCountryNameFromCode(user.country_code);
+    if (user.bio) 
+     bioDOM.textContent = user.bio;
+    collegeDOM.textContent = user.college_name;
+    usernameDOM.textContent = user.username;
+
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+fillUserDetails();
