9bea60fae8e85975c7e42012bf4b12e8551f3b85
VantaTree <vantatree@gmail.com>
2025-05-05 14:59:15 +0530

    - Formatted code, - Added a favicon link to the HTML head for better branding.

--- public/js/market.js
+++ public/js/market.js
@@ -1,6 +1,6 @@
 function toggleSidebar() {
-const sidebar = document.getElementById("sidebar");
-sidebar.classList.toggle("translate-x-full");
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("translate-x-full");
 }
 
 // function toggleFilterSidebar() {
@@ -10,38 +10,38 @@ sidebar.classList.toggle("translate-x-full");
 // }
 
 function toggleFilterSidebar() {
-  const sidebar = document.getElementById("filterSidebar");
-  sidebar.classList.toggle("-translate-x-full");
+    const sidebar = document.getElementById("filterSidebar");
+    sidebar.classList.toggle("-translate-x-full");
 }
 
 
 document.addEventListener("keydown", function (e) {
-if (e.key === "Escape") {
-document.getElementById("sidebar").classList.add("translate-x-full");
-document.getElementById("filterSidebar").classList.add("-translate-x-full");
-document.getElementById("filterSidebar").classList.remove("translate-x-0");
-}
+    if (e.key === "Escape") {
+        document.getElementById("sidebar").classList.add("translate-x-full");
+        document.getElementById("filterSidebar").classList.add("-translate-x-full");
+        document.getElementById("filterSidebar").classList.remove("translate-x-0");
+    }
 });
 
 
 function confirmLogout() {
-if (confirm("Are you sure you want to logout?")) {
-  window.location.href = "index.html";
-}
+    if (confirm("Are you sure you want to logout?")) {
+        window.location.href = "index.html";
+    }
 }
 
 document.querySelectorAll(".sidebar-link").forEach((link) => {
-link.addEventListener("click", function () {
-  document.getElementById("sidebar").classList.add("translate-x-full");
-});
+    link.addEventListener("click", function () {
+        document.getElementById("sidebar").classList.add("translate-x-full");
+    });
 });
 
 document.addEventListener("keydown", function (e) {
-if (e.key === "Escape") {
-  document.getElementById("sidebar").classList.add("translate-x-full");
-}
+    if (e.key === "Escape") {
+        document.getElementById("sidebar").classList.add("translate-x-full");
+    }
 });
 
 document.getElementById("price").addEventListener("input", function () {
-document.getElementById("price-value").textContent = this.value;
+    document.getElementById("price-value").textContent = this.value;
 });
